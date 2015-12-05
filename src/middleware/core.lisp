36abc2b210dcf5af895f49d8f4e670654c6caba0
Steve Losh <steve@stevelosh.com>
2015-12-05 20:35:38 +0000

    Move middleware utils to the middleware module

--- src/middleware/core.lisp
+++ src/middleware/core.lisp
@@ -2,3 +2,15 @@
 
 (defvar *middleware* (fset:empty-map))
 
+
+(defmacro handle-op (message op fallback &rest body)
+  `(if (equal ,op (fset:lookup ,message "op"))
+     (progn ,@body)
+     (funcall ,fallback ,message)))
+
+(defmacro define-middleware (name op message-binding &rest body)
+  (let ((fallback (gensym)))
+  `(defun ,name (,fallback)
+     (lambda (,message-binding)
+       (handle-op ,message-binding ,op ,fallback
+                  ,@body)))))
--- src/utils.lisp
+++ src/utils.lisp
@@ -97,14 +97,3 @@
                       "id" (fset:lookup message "id")
                       "session" (fset:lookup message "session"))))
 
-(defmacro handle-op (message op fallback &rest body)
-  `(if (equal ,op (fset:lookup ,message "op"))
-     (progn ,@body)
-     (funcall ,fallback ,message)))
-
-(defmacro define-middleware (name op message-binding &rest body)
-  (let ((fallback (gensym)))
-  `(defun ,name (,fallback)
-     (lambda (,message-binding)
-       (handle-op ,message-binding ,op ,fallback
-                  ,@body)))))
