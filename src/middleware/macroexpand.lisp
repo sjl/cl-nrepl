6082283602453a45a01253aba750edc84cb5b303
Steve Losh <steve@stevelosh.com>
2016-04-09 23:22:05 +0000

    Make macroexpand less ugly on CCL

--- src/middleware/macroexpand.lisp
+++ src/middleware/macroexpand.lisp
@@ -1,13 +1,20 @@
 (in-package #:nrepl)
 
+(defun pretty-string (form)
+  "Return a prettified string version of `form`, indented nicely."
+  #-ccl (format nil "~A" form)
+  ;; CCL's format doesn't indent forms nicely
+  #+ccl (with-output-to-string (*standard-output*)
+          (pprint form)))
+
 (define-middleware wrap-macroexpand "macroexpand" message
   ;; TODO: handle mangled input
   (let ((form (read-from-string (fset:lookup message "form"))))
     (respond message
              (make-map
                "status" '("done")
-               "macroexpand" (format nil "~A" (macroexpand form))
-               "macroexpand-1" (format nil "~A" (macroexpand-1 form))))))
+               "macroexpand" (pretty-string (macroexpand form))
+               "macroexpand-1" (pretty-string (macroexpand-1 form))))))
 
 
 
